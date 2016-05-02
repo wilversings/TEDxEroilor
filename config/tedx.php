0a83ccf498fb29d4180bf636754f26ba037f6e98
wilversings <aiordachioaei.marius@gmail.com>
2016-05-02 12:07:10 +0300

    Duplicated the language file for romanian

--- config/tedx.php
+++ config/tedx.php
@@ -5,7 +5,7 @@ return [
 
     'facebook_token' => "EAADBb4mzn18BAIGZCTZCmK32ZCBV55R2TDXdiPkzI2g4wPm15jq4xbNU1ZAoZAZA1ZAKzoZALcYNZCKdTIrTqyvnpVp7ZBFG1deGknf1f0VVEihZCxD7GExDRTQjwf1lZApopKG1ebnR5ZCOikaEDZBgKtSeoKRLr0LObVUJ8ZD",
     
-    // Pagina de youtube din care se iau ultimele postari de pe pagina home
+    // Pagina de facebook din care se iau ultimele postari de pe pagina home
     'facebook_pagename' => 'TEDxEroilor',
     
     'twitter_token' => "",
--- resources/lang/ro/strings.php
+++ resources/lang/ro/strings.php
@@ -2,15 +2,26 @@
 
 return [
 
-    //Navbar strings
-    'home' => 'Acasă',
-    'about' => 'Despre',
-    'speakers' => 'Speakeri',
+    /*----------------------------------------------
+    *
+    * Aici se gasesc stringurile care sunt traduse.
+    * ATENTIE, in nici un caz nu se traduc si nu se modifica sau editeaza cheile, ci doar valorile din array.
+    * 'cheie' => 'valoare', valorile se afla mereu in dreapta sagetii `=>` !
+    *
+    * Pentru a asocia ce se gaseste aici cu ce se afiseaza pe site, te poti lua dupa chei. Adica acestea au nume
+    * destul de sugestiv.
+    * 
+    ------------------------------------------------*/
+
+
+    // Navbar strings
+    'home' => 'Home',
+    'about' => 'About',
+    'speakers' => 'Speakers',
     'award' => 'Award',
     'contact' => 'Contact',
-    'partners' => 'Parteneri',
-
-     //Home
+    'partners' => 'Partners',
+    // Home
     'upcoming_event' => 'Upcoming Event',
     'days' => 'Days',
     'hours' => 'Hours',
@@ -25,18 +36,75 @@ return [
     'subscribe_form_email_placeholder' => 'Your Email Address',
     'subscribe_form_button' => 'Subscribe',
     
-    //About
+    // About
     
-    //Common
-    'team' => 'Echipă',
+    // Common
+    'team' => 'Team',
     'alumni' => 'Alumni',
-    'boa' => 'Consilui de adviseri',
+    'boa' => 'Broad of Advisers',
+    // Team
+    'head_top_title_teammember' => '(1){Romana} About team',
+    'head_title_teammember' => '(1){Romana} Our mission',
+    'head_p1_teammember' => '(1){Romana} TEDxEroilor is a local, independently organized TEDx event, operated under licence from TED. It wishes to promote TED concept in Cluj-Napoca, by organizing TEDx events',
+    'head_p2_teammember' => '(1){Romana} These are meant to bring together proactive individuals, who have the power to help spread those ideas that cand make Romania evolve in a positive direction.',
+    'content_title_teammember' => '(1){Romana} Our team',
+    // Alumni
+    'head_top_title_alumna' => '(2){Romana} About team',
+    'head_title_alumna' => '(2){Romana} Our mission',
+    'head_p1_alumna' => '(2){Romana} TEDxEroilor is a local, independently organized TEDx event, operated under licence from TED. It wishes to promote TED concept in Cluj-Napoca, by organizing TEDx events',
+    'head_p2_alumna' => '(2){Romana} These are meant to bring together proactive individuals, who have the power to help spread those ideas that cand make Romania evolve in a positive direction.',
+    'content_title_alumna' => '(2){Romana} Our team',
+    // Broad of Advisers
+    'head_top_title_adviser' => '(3){Romana} About team',
+    'head_title_adviser' => '(3){Romana} Our mission',
+    'head_p1_adviser' => '(3){Romana} TEDxEroilor is a local, independently organized TEDx event, operated under licence from TED. It wishes to promote TED concept in Cluj-Napoca, by organizing TEDx events',
+    'head_p2_adviser' => '(3){Romana} These are meant to bring together proactive individuals, who have the power to help spread those ideas that cand make Romania evolve in a positive direction.',
+    'content_title_adviser' => '(3){Romana} Our team',
     
-    //Team
-    'about_team' => 'Despre echipă',
-    'our_mission' => 'Misiunea noastră',
-    'our_mission_p1' => 'TEDxEroilor is a local, independently organized TEDx event, operated under licence from TED. It wishes to promote TED concept in Cluj-Napoca, by organizing TEDx events',
-    'our_mission_p2' => 'These are meant to bring together proactive individuals, who have the power to help spread those ideas that cand make Romania evolve in a positive direction.',
-    'our_team' => 'Echipa noastră',
+    // Speakers
+    'head_top_title' => 'Upcoming events',
+    'read_more_button' => 'Read more',
+    'next_event_speakers' => 'Next event speakers',
+    'previous_events_speakers' => 'Previous events speakers',
+   
+    
+    // Contact
+    'contact_us' => 'Contact Us',
+    'lets_chat' => 'Let\'s chat!',
+    'name' => 'Name',
+    'email' => 'Email',
+    'message' => 'Message',
+    'send_message' => 'Send message',
+    'locations' => [
+        'Sala studio',
+        'Fabrica de pensule', 
+        'Cluj-Napoca', 
+        'Strada Henri-Barbusse 59-61',
+        // Adauga cate vrei atata timp cat respecti conventia evidenta si sintaxa unui array PHP
+    ],
+    'contact_entry_accepted' => 'Formularul a fost inregistrat',
+    
+    // Award
+    'left_top_title' => 'TEDxEroilor Award',
+    'left_title' => 'Award 2016',
+    'left_paragraphs' => [
+        'Dorim să premiem inovațiile curajoase care au rolul de a schimba lumea în mai bine sau de a aduce noutăți nebănuite în lumea tehnologiei.',
+        'Am creat TEDxEroilor Award, un proiect prin care dorim să evidențiem și să premiem profesioniștii care inovează în domeniul lor de activiate in zona Transilvaniei.',
+        // Adauga cate vrei atata timp cat respecti conventia evidenta si sintaxa unui array PHP
+    ],
+    'right_banner' => 'View partners',
+    'steps_title' => 'Etape',
+    'apply_button' => 'Apply',
+    'steps' => [
+        ['date' => '12 - 28 Ianuarie', 'step' => 'Nominalizări'],
+        ['date' => '21 Ianuarie', 'step' => 'Selectarea celor mai bune zece nominalizări'],
+        ['date' => '1 - 13 Februarie', 'step' => 'Întâlniri de lucru împreună cu juriul pentru a discuta despre mentorat, dezvoltarea ideilor în afaceri și consilere financiară'],
+        ['date' => '13 - 16 Februarie', 'step' => 'Juriul va alege cele mai bune 3 inovații după procesul de mentorat.'],
+        ['date' => '20 Februarie', 'step' => 'Anunțarea inovației câștigătoare în cadrul conferinței TEDxEroilor'],
+        // Adauga cate vrei atata timp cat respecti conventia evidenta si sintaxa unui array PHP
+    ],
+    
+    // Partners
+    'partners' => 'Partners',
 
 ];
